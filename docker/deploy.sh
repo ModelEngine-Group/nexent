551af8aee1e678882f0770fde4e66928ffc7cd50
Jasonxia007 <iamjasonxia@126.com>
2026-02-05 09:10:08 +0800

    ♻️ Remove group tags in avatar dropdown

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -941,7 +941,7 @@ main_deploy() {
   echo "--------------------------------"
   echo ""
 
-  APP_VERSION="v1.7.9.2"
+  APP_VERSION="$(get_app_version)"
   if [ -z "$APP_VERSION" ]; then
     echo "❌ Failed to get app version, please check the backend/consts/const.py file"
     exit 1
