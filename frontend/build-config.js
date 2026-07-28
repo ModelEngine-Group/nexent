e0c28435da963d49ee32e8c54b912140cb1b1e41
xuyaqi <xuyaqist@gmail.com>
2026-07-28 10:07:56 +0800

    Bugfix: Unify modules to ES Module (#3516)

--- frontend/build-config.js
+++ frontend/build-config.js
@@ -1,6 +1,9 @@
-const fs = require("node:fs");
-const path = require("path");
+import fs from "node:fs";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const LOCALES_CONFIG_DIR = path.resolve(__dirname, "./public/locales");
 
 const defaultSize = 10;
