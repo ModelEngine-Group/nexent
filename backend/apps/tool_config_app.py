29f676fc116e762bb2a5844ed6b98e6a6d607388
HAN <837833609@qq.com>
2026-08-31 16:30:41 +0800

    恢复工具手动刷新逻辑

--- backend/apps/tool_config_app.py
+++ backend/apps/tool_config_app.py
@@ -112,6 +112,7 @@ async def scan_and_update_tool(
     """ Used to update the tool list and status """
     try:
         user_id, tenant_id = get_current_user_id(authorization)
+        await update_tool_list(tenant_id=tenant_id, user_id=user_id)
         return JSONResponse(
             status_code=HTTPStatus.OK,
             content={"message": "Successfully update tool", "status": "success"}
