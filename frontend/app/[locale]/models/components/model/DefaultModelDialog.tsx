301fef723db5f4dd72b5b2a1c5ff0bd3100a27e8
ljy <ljy@DESKTOP-65OBISN.(none)>
2026-09-15 19:25:21 +0800

    Style: widen default-model dialog to 1180px

--- frontend/app/[locale]/models/components/model/DefaultModelDialog.tsx
+++ frontend/app/[locale]/models/components/model/DefaultModelDialog.tsx
@@ -165,7 +165,7 @@ export const DefaultModelDialog = ({
       onOk={handleOk}
       okText={t("common.save", { defaultValue: "保存" })}
       cancelText={t("common.cancel", { defaultValue: "取消" })}
-      width={1000}
+      width={1180}
       centered
       destroyOnHidden
       styles={{ body: { paddingTop: 16 } }}
